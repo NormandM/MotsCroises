0d967ce9b5bb5bc63107058f529d99d713b736db
NormandM <nmartin1956@gmail.com>
2016-12-29 21:20:49 -0500

    Initial Commit

---
+++ Mots-Croisés-Classiques/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mots-Croisés-Classiques
+//
+//  Created by Normand Martin on 2016-12-29.
+//  Copyright © 2016 Normand Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
